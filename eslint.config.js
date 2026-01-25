9ac31c2e4f7d61fb3901728249022e589bef21b7
Aurora Kruschewsky <auro.kruschewsky@gmail.com>
2026-01-24 22:49:28 -0300

    chore: implements eslint to lint, prettier as formatter and husky to validate commits

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,44 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require("eslint-config-expo/flat");
+const { defineConfig } = require("eslint/config");
+const expo = require("eslint-config-expo/flat");
+const tseslint = require("@typescript-eslint/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
+const react = require("eslint-plugin-react");
+const reactNative = require("eslint-plugin-react-native");
+const prettier = require("eslint-config-prettier");
 
 module.exports = defineConfig([
-  expoConfig,
+  expo,
+
   {
-    ignores: ["dist/*"],
-  }
+    files: ["**/*.{ts,tsx,js,jsx}"],
+
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+
+    plugins: {
+      "@typescript-eslint": tseslint,
+      react,
+      "react-native": reactNative,
+    },
+
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactNative.configs.all.rules,
+      ...prettier.rules,
+
+      "react-native/no-unused-styles": "warn",
+      "react-native/no-inline-styles": "off",
+      "react-native/split-platform-components": "error",
+
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/explicit-module-boundary-types": "warn",
+    },
+  },
 ]);
